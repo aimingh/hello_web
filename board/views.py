398b0416de6d6e8a4d89432b8cb7bb0c6fe4f4e5
Junhee Cho <junhee3085@gmail.com>
2020-10-13 17:32:09 +0900

    add scrapping jobkorea

--- board/views.py
+++ board/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from pymongo import MongoClient
-from datas.scraping_worknet import scrapping_worknet
+from datas.scraping_jobsearch import scrapping_jobkorea, scrapping_worknet
 
 def listwithmongo(request):
     data = request.GET.copy()
@@ -25,3 +25,19 @@ def scrapworknet(request):
     keyword = request.GET['keyword']
     scrapping_worknet(max_page, keyword)
     return redirect('listofworknet_boardv')
+
+def listofjobkorea(request):
+    data = request.GET.copy()
+    with MongoClient('mongodb://127.0.0.1:27017') as client:
+        mydb = client.mydb
+        result = list(mydb.jobkorea.find({}))
+        data['page_obj'] = result
+    return render(request, 'board/listofjobkorea.html', context=data)
+
+def scrapjobkorea(request):
+    # with MongoClient('mongodb://127.0.0.1:27017') as client:
+    #     client.mydb.drop_collection('jobkorea')
+    max_page = int(request.GET['max_page'])
+    keyword = request.GET['keyword']
+    scrapping_jobkorea(max_page, keyword)
+    return redirect('listofjobkorea_boardv')
